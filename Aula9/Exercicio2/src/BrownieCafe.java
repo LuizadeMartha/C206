abc1d436c9a2a292324fe99ebc551ce95be5dc4e
LuizadeMartha <124800649+LuizadeMartha@users.noreply.github.com>
2023-04-11 14:30:18 -0300

    Add files via upload

---
+++ Aula9/Exercicio2/src/BrownieCafe.java
@@ -0,0 +1,15 @@
+public class BrownieCafe extends Brownie {
+
+    public BrownieCafe(String nome, double preco, String sabor) {
+        super(nome,preco,sabor);
+    }
+
+    public void adicionaCafe() {
+        System.out.println(super.nome +" adicionando mais café");
+    }
+    @Override
+    public void addCarrinhoDeCompras(){
+        System.out.println("Brownie de café sendo adicionado");
+    }
+
+}
---
+++ Aula9/Exercicio2/src/BrownieNutella.java
@@ -0,0 +1,16 @@
+public class BrownieNutella extends Brownie {
+
+    public BrownieNutella(String nome, double preco, String sabor) {
+        super(nome,preco,sabor);
+    }
+
+    public void adicionaNutella() {
+
+        System.out.println(super.nome +" adicionando mais nutella");
+    }
+    @Override
+    public void addCarrinhoDeCompras(){
+        System.out.println("Brownie de Nutela sendo adicionado");
+    }
+
+}
