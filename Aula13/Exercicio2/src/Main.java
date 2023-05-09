52aa7b5da39b70dc62887c5020e3d589c396cb2f
LuizadeMartha <124800649+LuizadeMartha@users.noreply.github.com>
2023-05-09 14:28:56 -0300

    Add files via upload

---
+++ Aula13/Exercicio2/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente cliente1 = new Cliente("Tio", 4321);
+        Cliente cliente2 = new Cliente("Tia", 1234);
+        Cliente cliente3 = null;
+
+        Conta conta = new Conta(1500, 500);
+
+        conta.addCliente(cliente1);
+        conta.addCliente(cliente2);
+        conta.addCliente(cliente3);
+
+        System.out.println("Mostra Info dos Clientes");
+        System.out.println();
+        conta.mostraInfo();
+        System.out.println("Tenta Sacar 1000");
+        try {
+            conta.sacar(1000);
+        } catch (SaldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println();
+        conta.mostraInfo();
+        System.out.println();
+
+        System.out.println("Tenta Sacar 1000");
+
+        try {
+            conta.sacar(1000);
+        } catch (SaldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println();
+        conta.mostraInfo();
+        System.out.println();
+
+        System.out.println("Fim da Main");
+
+
+        System.out.println("Fim da Main");
+
+    }
+
+}
